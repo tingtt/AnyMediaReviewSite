ed00dd3c85fe9437770c47053f9ade292eb67b91
yuuuuuuuuuuki42 <eccyuuki422@gmail.com>
2020-07-22 14:14:27 +0900

    first commit

--- AnyMediaReviewSite/src/register.php
+++ AnyMediaReviewSite/src/register.php
@@ -1,1 +1,16 @@
 <!--ユーザ登録-->
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>AMRS</title>
+        <meta charset="Shift_JIS">
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" href="register.css" media="all">
+    </head>
+
+    <body>
+        
+    
+    </body>
+</html>
--- AnyMediaReviewSite/src/review_detail.php
+++ AnyMediaReviewSite/src/review_detail.php
@@ -1,1 +1,15 @@
 <!--タイムラインで、レビューを押したら詳細表示する-->
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>AMRS</title>
+        <meta charset="Shift_JIS">
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" href="review_detail.css" media="all">
+    </head>
+
+    <body>
+        
+    </body>
+</html>
