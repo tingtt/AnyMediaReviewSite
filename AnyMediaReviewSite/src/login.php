3b3b89c2550a7743df887ed175080c0a929a1607
tingtt <t.t.launcher.real@gmail.com>
2020-07-27 16:57:52 +0900

    temp

--- AnyMediaReviewSite/src/login.php
+++ AnyMediaReviewSite/src/login.php
@@ -2,25 +2,25 @@
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>AMRS</title>
-        <meta charset="Shift_JIS">
-        <meta name="viewport" content="width=device-width">
-        <link rel="stylesheet" href="login.css" media="all">
-    </head>
 
-    <body>
-        <div id="form">
-    <center>
-        <h1 class="title">ログイン</h1>
-        <form action="menu.jsp" method="post">
-            <p class="un">UserID</p>
-            <p class="user"><input class="userID" type=text name=name placeholder=""></P>        <!--ユーザー名入力-->
-            <p class="pw">Password</p>
-            <p class="pass"><input class="password" type=password name=pass placeholder=""></P>  <!--パスワード入力-->
-            <input class="submit" type="submit" value="Login" />
-        </form>
-    </center>
+<head>
+    <title>AMRS - Login</title>
+    <link rel="stylesheet" href="login.css" media="all">
+</head>
+
+<body>
+    <div id="form">
+        <div class="center">
+            <h1 class="title">ログイン</h1>
+            <form action="login_check.php" method="post">
+                <!--ユーザー名入力-->
+                <p class="user"><input class="userID" type=text name=name placeholder="Username"></P>
+                <!--パスワード入力-->
+                <p class="pass"><input class="password" type=password name=pass placeholder="Password"></P>
+                <input class="submit" type="submit" value="Login" />
+            </form>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
