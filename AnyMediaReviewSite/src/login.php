355535c5a50904f8fa9d9a751415383c8a08cce4
tingtt <t.t.launcher.real@gmail.com>
2020-07-29 11:58:04 +0900

    login check complete ver 1

--- AnyMediaReviewSite/src/login.php
+++ AnyMediaReviewSite/src/login.php
@@ -1,5 +1,16 @@
 <!--ログイン-->
 
+<?php
+
+if (isset($_SESSION['user_id'])) {
+    $user_id = $_SESSION['user_id'];
+}
+
+if (isset($_GET['status'])) {
+    $status = $_GET['status'];
+}
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -14,10 +25,11 @@
             <h1 class="title">ログイン</h1>
             <form action="login_check.php" method="post">
                 <!--ユーザー名入力-->
-                <p class="user"><input class="userID" type=text name=name placeholder="Username"></P>
+                <p class="user"><input type=text name=id placeholder="UserID"><?= $user_id ?></p>
                 <!--パスワード入力-->
-                <p class="pass"><input class="password" type=password name=pass placeholder="Password"></P>
+                <p class="pass"><input type=password name=pass placeholder="Password"></p>
                 <input class="submit" type="submit" value="Login" />
+                <font color='red'><?= $status ?></font>
             </form>
         </div>
     </div>
