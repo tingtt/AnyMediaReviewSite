b6c360517f95c2a7b9e6011ee0e976cde3b7faad
tingtt <t.t.launcher.real@gmail.com>
2020-07-29 21:04:16 +0900

    complete ver 1

---
+++ AnyMediaReviewSite/src/regist_check.php
@@ -0,0 +1,52 @@
+<?php
+
+if (!isset($_POST['id'])) {
+    header('Location:register.php');
+}
+
+$input_id = htmlspecialchars($_POST['id']);
+$input_pass = htmlspecialchars($_POST['pass']);
+
+/**
+ * Undocumented function
+ *
+ * @param String $id
+ * @param String $pass
+ * @return array(Bool,String stasus)
+ */
+function verify_regist(String $id, String $pass)
+{
+    // connect
+    $dns = 'mysql:dbname=amrs;host=127.0.0.1';
+    $user = 'root';
+    $password = '';
+    try {
+        $db_handle = new PDO($dns, $user, $password);
+    } catch (PDOexception $th) {
+        return array(false, 'Error: Cannot connect DB');
+    }
+
+    $sql = 'SELECT * FROM accounts WHERE id = :id';
+    $prepare = $db_handle->prepare($sql);
+    $prepare->bindValue(':id', $id, PDO::PARAM_STR);
+
+    $prepare->execute();
+
+    $row_num = $prepare->rowCount();
+
+    if ($row_num == 0) {
+        return array(true, '');
+    } else {
+        return array(false, 'Used ID entered.');
+    }
+}
+
+$verify = verify_login($input_id, $input_pass);
+
+if ($verify[0]) {
+    $_SESSION['user_id'] = $input_id;
+    $_SESSION['password'] = $input_pass;
+    header('Location:account_settings.php');
+} else {
+    header('Location:register.php?status=' . $verify[1]);
+}
--- AnyMediaReviewSite/src/register.php
+++ AnyMediaReviewSite/src/register.php
@@ -1,1 +1,28 @@
 <!--ユーザ登録-->
+
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>AMRS - Register</title>
+</head>
+
+<body>
+    <h1 id="title">Register</h1>
+    <div class="form">
+        <form action="regist_check.php" method="post">
+            <input type="text" name="id" placeholder="User ID"><?= $user_id ?><br>
+            <input type="password" name="pass" placeholder="Password"><br>
+            <input type="password" name="pass_check" placeholder="Password verify"><br>
+            <font color='red'><?= $status ?></font><br>
+            <input type="submit" value="Regist">
+            <a href="login.php">Login</a>
+        </form>
+    </div>
+</body>
+
+</html>
