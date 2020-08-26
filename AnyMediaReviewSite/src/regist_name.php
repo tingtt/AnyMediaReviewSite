9f21b9737918af57f15fc25caef9ebc598ea7294
tingtt <t.t.launcher.real@gmail.com>
2020-08-26 13:54:43 +0900

    create account flow

--- AnyMediaReviewSite/src/regist_name.php
+++ AnyMediaReviewSite/src/regist_name.php
@@ -1,31 +1,14 @@
 <?php
+session_start();
 
-if (!isset($_SESSION['user_id']) || !isset($_SESSION['password'])) {
+if (!isset($_SESSION['user_id'])) {
     header('Location:register.php');
 }
 
-function insert_account_info(String $id, String $name, String $pass)
-{
-    // connect
-    $dns = 'mysql:dbname=amrs;host=127.0.0.1';
-    $user = 'root';
-    $password = '';
-    try {
-        $db_handle = new PDO($dns, $user, $password);
-    } catch (PDOexception $th) {
-        return array(false, 'Error: Cannot connect DB');
-    }
-
-    $sql = 'INSERT INTO accounts (id, name, pass) VALUES(?, ?, ?)';
-    $stmt = $db_handle->prepare($sql);
-
-    $flag = $stmt->execute(array($id, $name, $pass));
-
-    if (!$flag) {
-        header('Location:register.php?status=' . "Error: Cannot registration.");
-    }
+if (isset($_POST['name'])) {
+    $_SESSION['name'] = htmlspecialchars($_POST['name']);
+    header('Location:regist_pass.php');
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -38,11 +21,11 @@ function insert_account_info(String $id, String $name, String $pass)
 
 <body>
     <div class="content">
-        <form action="regist_name" method="post">
+        <form action="regist_name.php" method="post">
             <h3>名前を入力してください。</h3>
-            <input type="text" placeholder="Name">
+            <input type="text" placeholder="Name" name="name">
             <button type="submit">決定</button>
-            <p>※ ここで設定した名前はサイトを利用する時に他のユーザにも表示されます。</p>
+            <p>※ ここで設定した名前は当サイトを使用するときに他のユーザにも表示され、変更可能です。</p>
         </form>
     </div>
 </body>
--- AnyMediaReviewSite/src/register.php
+++ AnyMediaReviewSite/src/register.php
@@ -1,9 +1,51 @@
-<!--ユーザ登録-->
-
 <?php
+session_start();
 
-if (session_status() == PHP_SESSION_DISABLED) {
-    session_start();
+// post送信されている場合の処理
+if (isset($_POST['id'])) {
+    $input_id = $_POST['id'];
+
+    /**
+     * IDが使用可能か確認
+     *
+     * @param String $id
+     * @return array(Bool,String stasus)
+     */
+    function verify_regist(String $id)
+    {
+        // connect
+        $dns = 'mysql:dbname=amrs;host=127.0.0.1';
+        $user = 'root';
+        $password = '';
+        try {
+            $db_handle = new PDO($dns, $user, $password);
+        } catch (PDOexception $th) {
+            return array(false, 'Error: Cannot connect DB');
+        }
+
+        $sql = 'SELECT * FROM accounts WHERE id = ?';
+        $prepare = $db_handle->prepare($sql);
+
+        $prepare->execute(array($id));
+
+        $row_num = $prepare->rowCount();
+
+        if ($row_num == 0) {
+            return array(true, '');
+        } else {
+            return array(false, 'Entered ID already used.');
+        }
+    }
+
+    $verify = verify_regist($input_id);
+
+    $_SESSION['user_id'] = $input_id;
+
+    if ($verify[0]) {
+        header('Location:regist_name.php');
+    } else {
+        $status = $verify[1];
+    }
 }
 
 if (isset($_SESSION['user_id'])) {
@@ -13,7 +55,6 @@ if (isset($_SESSION['user_id'])) {
 if (isset($_GET['status'])) {
     $status = $_GET['status'];
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -27,34 +68,13 @@ if (isset($_GET['status'])) {
 <body>
     <h1 id="title">Register</h1>
     <div class="form">
-        <form action="regist_check.php" method="post">
-            <input type="text" name="id" placeholder="User ID"><?= $user_id ?><br>
-            <input id="pass1" type="password" name="pass" placeholder="Password"><br>
-            <input id="pass2" type="password" name="pass_check" placeholder="Password verify"><br>
+        <form action="register.php" method="post">
+            <input type="text" name="id" placeholder="User ID" value="<?= $user_id ?>"><br>
             <font id="status" color='red'><?= $status ?></font><br>
-            <input id="submit" type="submit" value="Regist">
+            <input type="submit" value="Regist">
             <a href="login.php">Login</a>
         </form>
     </div>
 </body>
 
 </html>
-
-<script>
-    // パスワード 2が変更されたらパスワード １と一致してるかチェック
-    const pass1 = document.getElementById("pass1");
-    const pass2 = document.getElementById("pass2");
-    const status = document.getElementById("status");
-    const submit = document.getElementById("submit");
-    pass2.addEventListener('change', () => {
-        if (pass1.value != pass2.value) {
-            console.log("dismatch");
-            status.innerHTML = "Password not match.";
-            submit.disabled = true;
-        } else {
-            console.log("match");
-            status.innerHTML = "";
-            submit.disabled = false;
-        }
-    });
-</script>
